2de5b417153f7db6d5fb98b361553811c12a7c88
AgusB1101 <agustinbernal69@gmail.com>
2021-11-17 15:10:23 -0300

    Changed server port and added root colors in css

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const session = require("express-session");
 const app = express();
 
-const PORT = 3000 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 
 console.clear();
 
